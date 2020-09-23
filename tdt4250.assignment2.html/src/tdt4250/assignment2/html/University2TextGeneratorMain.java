5e5572234029f0cab3060dc9b77f92a931d62b05
Mari Sofie Lerfaldet <marilerfaldet@gmail.com>
2020-09-23 21:02:33 +0200

    deleted unused class

--- tdt4250.assignment2.html/src/tdt4250/assignment2/html/University2TextGeneratorMain.java
+++ tdt4250.assignment2.html/src/tdt4250/assignment2/html/University2TextGeneratorMain.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
+import studyprograms.StudyprogramsPackage;
 import studyprograms.University;
+import studyprograms.impl.StudyprogramsFactoryImpl;
 import tdt4250.assignment2.html.acceleo.University2TextGenerator;
 
 public class University2TextGeneratorMain {
 
 	public static void main(String[] args) throws IOException {
-		University university = (args.length > 0 ? University2XhtmlGeneratorMain.getUniversity(args[0]) : University2XhtmlGeneratorMain.getSampleUniversity());
+		University university = (args.length > 0 ? getUniversity(args[0]) : getSampleUniversity());
 		//TODO: Find out what is happening with generatehtml -- cannot find in example repo from Hallvard	
 		String html = new University2TextGenerator().generateHtml(university);
 		if (args.length > 1) {
@@ -21,6 +27,28 @@ public class University2TextGeneratorMain {
 			}
 		} else {
 			System.out.println(html);
+		}
+	}
+	
+	public static University getUniversity(String uriString) throws IOException {
+		ResourceSet resSet = new ResourceSetImpl();
+		resSet.getPackageRegistry().put(StudyprogramsPackage.eNS_URI, StudyprogramsPackage.eINSTANCE);
+		resSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("university", new StudyprogramsFactoryImpl());
+		Resource resource = resSet.getResource(URI.createURI(uriString), true);
+		for (EObject eObject : resource.getContents()) {
+			if (eObject instanceof University) {
+				return (University) eObject;
+			}
+		}
+		return null;
+	}
+	
+	public static University getSampleUniversity() {
+		try {
+			return getUniversity(University2TextGeneratorMain.class.getResource("SampleUniversity.university").toString());
+		} catch (IOException e) {
+			System.err.println(e);
+			return null;
 		}
 	}
 }
